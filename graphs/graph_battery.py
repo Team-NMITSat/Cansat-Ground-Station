c1576203e815ef76f7c417c643eded278cc8a92f
aryan <agarwalaryan139@gmail.com>
2024-03-01 21:17:38 +0530

    fix: bttery

--- graphs/graph_battery.py
+++ graphs/graph_battery.py
@@ -1,16 +1,19 @@
 import pyqtgraph as pg
 
+
 class graph_battery(pg.PlotItem):
-    
-    def __init__(self, parent=None, name=None, labels=None, title='Battery staus', viewBox=None, axisItems=None, enableMenu=True,value=84, font = None,**kargs):    
+        
+    def __init__(self, parent=None, name=None, labels=None, title='Date', viewBox=None, axisItems=None, enableMenu=True, font = None,**kargs):
         super().__init__(parent, name, labels, title, viewBox, axisItems, enableMenu, **kargs)
 
         self.hideAxis('bottom')
         self.hideAxis('left')
-        self.battery_text = pg.TextItem(str(value), anchor=(0.5, 0.5), color="w")
+        self.time_text = pg.TextItem("test", anchor=(0.5, 0.5), color="w")
         if font != None:
-            self.battery_text.setFont(font)
-        self.addItem(self.battery_text)
+            self.time_text.setFont(font)
+        self.addItem(self.time_text)
 
-    def update(self, value_chain):
-        pass
+
+    def update(self, value):
+        self.time_text.setText('')
+        self.time_text.setText(str(value))
